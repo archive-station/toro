79c004c557ba1daa144d14d8f2f5016514a6ffe0
ellie <81702700+yeeterlol@users.noreply.github.com>
2024-02-15 17:31:40 -0800

    make proxy downloads work

--- handler/server.js
+++ handler/server.js
@@ -1,3 +1,6 @@
+import config from "../servers.json" assert { type: "json" };
+import { serveDir, serveFile } from "https://deno.land/std@0.207.0/http/file_server.ts";
+
 export async function handleRequest(_req) {
     const url = new URL(_req.url);
     const path = url.pathname;
@@ -16,31 +19,61 @@ export async function handleRequest(_req) {
 
     // prevent no errors lol!!!!!! :3
 
-    const filePath = await Deno.realPath(`./archive.${url.pathname}`);
-
-    console.log(filePath)
+    
+    // console.log(filePath)
     // const filePath = path.join(__dirname, './backup', '.'+originalUrl)
     let file;
     if (_req.method === "GET") {
         try {
+            const filePath = await Deno.realPath(`./archive.${url.pathname}`);
             const fileCheck = Deno.statSync(filePath);
             if (fileCheck.isDirectory) {
                 throw new Error();
             }
 
             
-            file = await Deno.open(filePath, {read: true});
-            
-            
-            
-            console.log('complete!')
+           return serveFile(_req, filePath);
         } catch (err) {
             // oh hell no!
         }
     }
-    const readableStream = file.readable;
-    const res = new Response(readableStream);
-    return await res;
+    
+    
+    let resHeaders = new Headers();
+    if (!file) {
+        let responseRes;
+    
+        let request;
+        try {
+            let current_servers = config.servers;
+            let current_server = current_servers[Math.floor(Math.random()*current_servers.length)];
+            
+            request = await fetch(`http://${current_server}/weeklytoro/pd/ps3${url.pathname}`, {headers: _req.headers});
+            let buff = await request.arrayBuffer();
+
+            if (!request.ok) {
+                throw new Error();
+            }
+            console.log(`http://${current_server}/weeklytoro/pd/ps3${url.pathname}`);
+            let arr = new Uint8Array(buff);
+            await Deno.writeFile(`./archive${url.pathname}`, arr);
+        } catch (err) {
+            if (request.status > 399) {
+                console.log('something fucked up!!!!!!!')
+            }
+        }
+    
+    
+        for (const header in request.headers) {
+            resHeaders.set(header, request.headers.get(header));
+        }
+    
+        if (request.status > 399) {
+            return await new Response('toro - the proxy can not find the file!', {status: 404});
+        } else {
+            return serveFile(_req, `./archive${url.pathname}`);
+        }
+    } 
 }
 
 export default handleRequest;
